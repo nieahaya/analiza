b21c9161688fa18c11c2fa1a5935b7e80b53dac7
nieahaya <aczacha1@gmail.com>
2024-12-02 11:56:14 +0100

    wiecej zadan

---
+++ 17.11/zmienne2.py
@@ -0,0 +1,6 @@
+#Napisz program w Pythonie, który przyjmuje od użytkownika imię i nazwisko, a następnie wyświetla je
+#w odwrotnej kolejności ze spacją między nimi i powitaniem, przydatne: input(), formatowanie wyświetlania
+
+imie=input("Podaj imię")
+nazwisko=input("Podaj nazwisko")
+print(f"Cześć {nazwisko} {imie}")
