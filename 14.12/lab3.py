81b2ac37daeca5457f404352d227720b2615c0ba
nieahaya <aczacha1@gmail.com>
2025-01-20 14:05:54 +0100

    20.01

--- 14.12/lab3.py
+++ 14.12/lab3.py
@@ -3,15 +3,15 @@
 #z modułu collections.
 #Użyteczne wyrażenia: collections.Counter(), most_common(), random.randrange().
 
-import random
-import collections
+#import random
+#import collections
 
-def zad1():
-    lista = [random.randrange(0, 11) for _ in range(50)]
-    print(lista)
-    return collections.Counter(lista).most_common(5)
+#def zad1():
+#    lista = [random.randrange(0, 11) for _ in range(50)]
+#    print(lista)
+#    return collections.Counter(lista).most_common(5)
 
-print(f"5 najczęściej spotykanych liczb: {zad1()}")
+#print(f"5 najczęściej spotykanych liczb: {zad1()}")
 
 
 #Zastosuj funkcje z modułu itertools, żeby wypisać na ekran permutacje i kombinacje dwusymbolowe, które
@@ -21,13 +21,13 @@ print(f"5 najczęściej spotykanych liczb: {zad1()}")
 #Kombinacje: AB AC AD BC BD CD
 #Użyteczne wyrażenia: itertools.permutations(), itertools.combinations(), input().
 
-import itertools
+#import itertools
 
-napis2=input("Podaj napis: ")
-permutacje=itertools.permutations(napis2, 2)
-kombinacje=itertools.combinations(napis2, 2)
-print("Permutacje:", " ".join([''.join(p) for p in permutacje]))
-print("Kombinacje:", " ".join([''.join(c) for c in kombinacje]))
+#napis2=input("Podaj napis: ")
+#permutacje=itertools.permutations(napis2, 2)
+#kombinacje=itertools.combinations(napis2, 2)
+#print("Permutacje:", " ".join([''.join(p) for p in permutacje]))
+#print("Kombinacje:", " ".join([''.join(c) for c in kombinacje]))
 
 
 #Utwórz strukturę składającą się z list zagnieżdżonych, która będzie reprezentować macierz (w rozumieniu
@@ -35,6 +35,7 @@ print("Kombinacje:", " ".join([''.join(c) for c in kombinacje]))
 #pionowy (reprezentowany przez zwykłą listę). W przypadku gdy do funkcji jako drugi argument jest podana
 #macierz (lista zagnieżdżona) zamiast listy, należy wyrzucić wyjątek ValueError w którym będą umieszczone
 #stosowne informacje. Zademonstruj działanie zaimplementowanej funkcji.
+
 #Listing 2: Przykład definiowania macierzy i przykładowy wynik działania programu.
 #Przykładowa macierz:
 #a = [
@@ -44,6 +45,137 @@ print("Kombinacje:", " ".join([''.join(c) for c in kombinacje]))
 #]
 ## Przykładowy wektor:
 #b = [6, 4, 2]
-## Wynik mnożenia macierzy a przez pionowy wektor b:
+#Wynik mnożenia macierzy a przez pionowy wektor b:
 #[20, 56, 92]
+
 #Użyteczne wyrażenia: def, return, for, range(), ValueError()
+
+def zad3(matrix, vector):
+    # Sprawdź, czy `vector` jest jednowymiarową listą
+    if any(isinstance(item, list) for item in vector):
+        raise ValueError("Drugi argument musi być wektorem (jednowymiarową listą).")
+
+    # Sprawdź, czy liczba kolumn w macierzy pasuje do długości wektora
+    if len(matrix[0]) != len(vector):
+        raise ValueError("Nieprawidłowe wymiary: liczba kolumn w macierzy musi być równa długości wektora.")
+
+    result = []
+    for row in matrix:
+        row_result = sum(row[i] * vector[i] for i in range(len(vector)))
+        result.append(row_result)
+
+    return result
+
+d = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+e = [6, 4, 2]
+
+try:
+    result = zad3(d,e)
+    print("Zadanie 3: ", result)
+except ValueError as e:
+    print("Błąd: ", e)
+
+
+#Wczytaj plik points.txt, który zawiera współrzędne 100 punktów położonych w przestrzeni dwuwymiarowej
+#(jeden wiersz - jeden punkt, współrzędne x i y są oddzielone od siebie spacjami). Następnie, wypisz na ekran
+#dwa punkty które są położone najbliżej siebie.
+#Użyteczne wyrażenia: with, open(), read(), splitlines(), math.dist(), itertools.combinations().
+
+import math
+
+# Funkcja do znajdowania dwóch najbliższych punktów
+def zad4(file_path):
+    # Wczytaj punkty z pliku
+    with open(file_path, "r") as plik:
+        punkty = []
+        for linia in plik:
+            x, y = map(float, linia.split())  # Zamień tekst na liczby
+            punkty.append((x, y))  # Dodaj punkt do listy
+
+    # Przygotowanie zmiennych do szukania najbliższych punktów
+    najblizsza_odleglosc = float("inf")  # Bardzo duża liczba na start
+    najblizsze_punkty = None
+
+    # Sprawdzaj każdą parę punktów
+    for i in range(len(punkty)):
+        for j in range(i + 1, len(punkty)):
+            p1 = punkty[i]
+            p2 = punkty[j]
+            # Oblicz odległość
+            odleglosc = math.sqrt((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2)
+            # Jeśli odległość jest mniejsza, zapisz te punkty
+            if odleglosc < najblizsza_odleglosc:
+                najblizsza_odleglosc = odleglosc
+                najblizsze_punkty = (p1, p2)
+
+    return najblizsze_punkty, najblizsza_odleglosc
+
+# Wykonaj program
+try:
+    punkty, odleglosc = zad4("points.txt")
+    print(f"Najbliższe punkty: {punkty[0]} i {punkty[1]}")
+    print(f"Odległość między nimi: {odleglosc}")
+except FileNotFoundError:
+    print("Nie znaleziono pliku 'points.txt'.")
+except ValueError:
+    print("Błąd: Plik ma niepoprawny format.")
+
+
+#Napisz funkcję, która służy do rozwiązywania równań kwadratowych przyjmującą trzy argumenty, będące współczynnikami
+#równania. Do obliczenia pierwiastka z delty należy użyć funkcji math.sqrt(). Proszę nie używać warunków dla
+#sprawdzenia czy delta jest ujemna, a wyjątek wyrzucany przez funkcje math.sqrt() przechwycić poza funkcją.
+
+#Następnie, napisz program który będzie w pętli odpytywać użytkownika o kolejne równania które trzeba rozwiązać
+#(użytkownik ma podawać trzy współczynniki w jednym wierszu, lub podawać kolejne współczynniki w kolejnych wierszach,
+#dane pobierać do momentu wprowadzenia przez użytkownika pustego wierszu). Znalezione pierwiastki należy wypisać na
+#ekran, jeżeli równanie nie ma pierwiastków rzeczywistych, odpowiednio poinformować użytkownika o tym. Proszę również
+#pamiętać, że jeżeli funkcja została zaimplementowana zgodnie z wymaganiami będzie ona wyrzucać wyjątek, który należy
+#odpowiednio obsłużyć.
+#Użyteczne wyrażenia: input(), math.sqrt(), try, expect.
+
+import math
+
+def funkcja_kwadratowa(a, b, c):
+    delta = b**2 - 4*a*c
+    x1 = (-b - math.sqrt(delta)) / (2*a)
+    x2 = (-b + math.sqrt(delta)) / (2*a)
+    return x1, x2
+
+#def zad5():
+    while True:
+        try:
+            input_data = input("Podaj współczynniki a, b, c (lub naciśnij Enter, aby zakończyć): ")
+            if not input_data:
+                break
+
+            input_data = input_data.replace(',', ' ')
+            # Rozdzielanie danych na trzy współczynniki
+            a, b, c = map(float, input_data.split())
+
+            try: 
+                print(f"Pierwiastki równania to: {funkcja_kwadratowa(a, b, c)}")
+            except ValueError:
+                print("Brak pierwiastków rzeczywistych")
+        except ValueError:
+            print("Błąd: Proszę podać trzy liczby oddzielone spacjami lub przecinkami.")
+
+#zad5()
+
+
+
+#Zaimplementuj funkcje, która utworzy listę liczb pierwszych do n (n jest argumentem funkcji) korzystając z algorytmu sita
+#Eratostenesa. Następnie, napisz program, który wygeneruje za pomocą tej funkcji listę liczb pierwszych do 100 i zapisze ją
+#do pliku prime_numbers.txt, zapisując po 5 kolejnych liczb pierwszych w każdym wierszu pliku wynikowego.
+#Więcej o sicie Eratostenesa: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes.
+#Użyteczne wyrażenia: with, open(), write().
+
+
+
+
+    
+
